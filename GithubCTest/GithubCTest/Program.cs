0d6c4021c01f9a24c6ecc3c8753fe2df99f59efe
yagote <3525013968@qq.com>
2021-05-22 10:23:45 +0800

    githubTest

---
+++ GithubCTest/GithubCTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubCTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Can You See Me?");
+            Console.ReadKey();
+        }
+    }
+}
